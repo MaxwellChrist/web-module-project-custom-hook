40302a82c24eff09819966e1ccdb7838c52898f9
MaxwellChrist <ribthulu@gmail.com>
2022-04-13 16:19:43 -0400

    cleaned up code and completed MVP

--- frontend/hooks/useDarkMode.js
+++ frontend/hooks/useDarkMode.js
@@ -1,6 +1,3 @@
-// Build a hook that ONLY setups a boolean state value. This value governs if dark mode is on or off.
-//  Return the slice of state created and the state modification function.
-//  Replace the darkMode state definition with useDarkMode in App.js.
 import React, { useState } from 'react';
 import useLocalStorage from './useLocalStorage';
 
--- frontend/hooks/useLocalStorage.js
+++ frontend/hooks/useLocalStorage.js
@@ -1,11 +1,7 @@
-// Build a hook called that takes in a key value and an initialValue.
-// Use the code from GP to build the the hook.
-
 import React, { useState } from 'react';
 
 const useLocalStorage = (key, value) => {
     const [newValue, setNewValue] = useState(() => {
-        // debugger
         if (localStorage.getItem(key)) {
             return JSON.parse(localStorage.getItem(key))
         }
